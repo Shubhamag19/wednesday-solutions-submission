b0e662693368a34dcac697e782e726b0ae7f6987
Shubham Agrawal <shubhamagrawal154@gmail.com>
2022-02-01 00:00:38 +0530

    Add files via upload

---
+++ app/components/RepoCard/index.js
@@ -0,0 +1,75 @@
+/**
+ *
+ * RepoCard
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Card } from 'antd';
+import T from '@components/T';
+import If from '@components/If';
+import { isEmpty } from 'lodash';
+
+const CustomCard = styled(Card)`
+  && {
+    margin: 20px 0;
+  }
+`;
+
+export function RepoCard({ artistName, primaryGenreName, trackName, trackPrice }) {
+  return (
+    <CustomCard data-testid="repo-card">
+      <If condition={!isEmpty(artistName)} otherwise={<T data-testid="name-unavailable" id="artistName_unavailable" />}>
+        <T data-testid="name" id="artist_name" values={{ artistName: artistName }} />
+      </If>
+      <If
+        condition={!isEmpty(primaryGenreName)}
+        otherwise={<T data-testid="fullName-unavailable" id="primaryGenreName_unavailable" />}
+      >
+        <T data-testid="fullName" id="primary_genre_name" values={{ genreName: primaryGenreName }} />
+      </If>
+      <If condition={trackName} otherwise={<T data-testid="stargazers-unavaiable" id="trackName_unavailable" />}>
+        <T data-testid="stargazers" id="track_name" values={{ trackName: trackName }} />
+      </If>
+      <If condition={trackPrice} otherwise={<T data-testid="stargazers-unavaiable" id="trackPrice_unavailable" />}>
+        <T data-testid="stargazers" id="track_price" values={{ trackPrice: trackPrice }} />
+      </If>
+    </CustomCard>
+  );
+}
+
+// export function RepoCard({ name, fullName, stargazersCount }) {
+//   return (
+//     <CustomCard data-testid="repo-card">
+//       <If condition={!isEmpty(name)} otherwise={<T data-testid="name-unavailable" id="repo_name_unavailable" />}>
+//         <T data-testid="name" id="repository_name" values={{ name: name }} />
+//       </If>
+//       <If
+//         condition={!isEmpty(fullName)}
+//         otherwise={<T data-testid="fullName-unavailable" id="repo_full_name_unavailable" />}
+//       >
+//         <T data-testid="fullName" id="repository_full_name" values={{ fullName: fullName }} />
+//       </If>
+//       <If condition={stargazersCount} otherwise={<T data-testid="stargazers-unavaiable" id="repo_stars_unavailable" />}>
+//         <T data-testid="stargazers" id="repository_stars" values={{ stars: stargazersCount }} />
+//       </If>
+//     </CustomCard>
+//   );
+// }
+
+// RepoCard.propTypes = {
+//   name: PropTypes.string,
+//   fullName: PropTypes.string,
+//   stargazersCount: PropTypes.number
+// };
+
+RepoCard.propTypes = {
+  artistName: PropTypes.string,
+  primaryGenreName: PropTypes.string,
+  trackName: PropTypes.string,
+  trackPrice: PropTypes.number
+};
+
+export default RepoCard;
